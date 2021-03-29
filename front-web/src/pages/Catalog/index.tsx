6957f42b9a0cb1bb906578e12add4855ad97ce78
marlon-frazao <frazao.marlon@gmail.com>
2021-03-29 15:00:54 -0300

    Dynamically listing products

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 
 const Catalog = () => {
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
 
     useEffect(() => {
         const params = {
@@ -14,7 +16,7 @@ const Catalog = () => {
         }
 
         makeRequest({ url: '/products', params })
-            .then(response => console.log(response));
+            .then(response => setProductsResponse(response.data));
     }, []);
 
     return (
@@ -23,17 +25,11 @@ const Catalog = () => {
                 Catálogo de produtos
             </h1>
             <div className="catalog-products">
-                <Link to="/products/1"><ProductCard /></Link>
-                <Link to="/products/2"><ProductCard /></Link>
-                <Link to="/products/3"><ProductCard /></Link>
-                <Link to="/products/4"><ProductCard /></Link>
-                <Link to="/products/5"><ProductCard /></Link>
-                <Link to="/products/6"><ProductCard /></Link>
-                <Link to="/products/7"><ProductCard /></Link>
-                <Link to="/products/8"><ProductCard /></Link>
-                <Link to="/products/9"><ProductCard /></Link>
-                <Link to="/products/10"><ProductCard /></Link>
-                <Link to="/products/11"><ProductCard /></Link>
+                {productsResponse?.content.map(product => (
+                    <Link to="/products/1" key={product.id}>
+                        <ProductCard />
+                    </Link>
+                ))}
             </div>
         </div>
     );
