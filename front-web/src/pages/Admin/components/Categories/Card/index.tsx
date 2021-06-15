d2ebdf8f608d05e961dbf0ec3d5f8cb530c59b01
marlon-frazao <frazao.marlon@gmail.com>
2021-06-15 10:19:29 -0300

    Category list layout

--- front-web/src/pages/Admin/components/Categories/Card/index.tsx
+++ front-web/src/pages/Admin/components/Categories/Card/index.tsx
@@ -1,5 +1,6 @@
 import { Category } from 'core/types/Category';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 type Props = {
@@ -11,8 +12,24 @@ const Card = ({ category, onRemove }: Props) => {
     return (
         <div className="card-base category-card-admin">
             <div className="row">
-                <div className="col-2 text-center border-right py-3">
+                <div className="col-6 text-left py-2">
                     {category.name}
+                </div>
+                <div className="col-6 d-flex">
+                    <Link
+                        to={`/admin/category/${category.id}`}
+                        type="button"
+                        className="btn btn-outline-secondary border-radius-10 category-btn"
+                    >
+                        EDITAR
+                    </Link>
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger border-radius-10 category-btn margin-left-30"
+                        onClick={() => onRemove(category.id)}
+                    >
+                        EXCLUIR
+                    </button>
                 </div>
             </div>
         </div>
